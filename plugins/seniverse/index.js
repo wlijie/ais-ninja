72fa8178334397e5ff12ebb76bebfa535684d20c
wlijie <493258141@qq.com>
2023-07-06 10:09:53 +0800

    Update index.js

--- plugins/seniverse/index.js
+++ plugins/seniverse/index.js
@@ -1,7 +1,7 @@
 
 async function search_weather({location}) {
   ais_progress(`searching ${location}`)
-  return await fetch(`https://api.seniverse.com/v3/weather/daily.json?key=${process.env.SENIVERSE_API_KEY}&location=${location}&language=zh-Hans&unit=c&start=-1&days=5`)
+  return await fetch(`https://api.seniverse.com/v3/weather/daily.json?key=4r9bergjetiv1tsd&location=${location}&language=zh-Hans&unit=c&start=-1&days=5`)
     .then(res => res.json());
 }
 
